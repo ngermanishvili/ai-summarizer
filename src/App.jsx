d1d3885919cd962a7714ef9b59bfd2b339583d40
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-04 21:23:31 +0400

    The Apply is not working for dynamic import inside components folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+
+
+const App = () => {
+  return (
+    <div>
+        <h1>React 18 Alpha</h1>
+        <p>React 18 Alpha is here!</p>
+    </div>
+  )
+}
+
+export default App
