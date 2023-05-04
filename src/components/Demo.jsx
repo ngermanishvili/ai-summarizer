98657c74d10228417d1263cccd1169bca4943bbf
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-04 21:49:24 +0400

    Add Input in Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
   return (
-    <div>Demo</div>
-  )
-}
+    <section className="mt-16 w-full max-w-xl flex justify-center ">
+      {/* search */}
+      <div className="flex flex-col w-full gap-2">
+        <form className="relative flex justify-center items-center">
+          <img
+            src={linkIcon}
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
 
-export default Demo
+          <input
+            type="url"
+            placeholder="Paste the article link"
+            value=""
+            onChange={() => {}}
+            required
+            className=" block w-full rounded-md border:border-gray-200 bg-white py-2.5 pl-10 pr-12 text-sm shadow-lg font-satoshi font-medium focus: border-black focus:outline-none focus:ring-0 peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
+          >
+            <p>↵</p>
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
