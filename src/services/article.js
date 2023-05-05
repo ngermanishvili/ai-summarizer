e5c767032ee4388206c0d92c24c45cf60039ab5d
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-05 19:52:59 +0400

    Update article API configuration to use RapidAPI base URL and headers

--- src/services/article.js
+++ src/services/article.js
@@ -1,6 +1,33 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const options = {
+    method: 'GET',
+    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+    params: {
+        url: 'https://time.com/6266679/musk-ai-open-letter/',
+        length: '3'
+    },
+    headers: {
+        'X-RapidAPI-Key': '6205bd2fa6mshe1b309c181be10ep1a0fcfjsn40ca06db33bf',
+        'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+    }
+};
+
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'KEY')
+            headers.set('X-RapidAPI-Host', 'KEY')
+        }
+
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            query: (params) => `test`
+        })
+    })
 
 })
