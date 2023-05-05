665c7eb328eea01f59c764e733dca54427b8a0c6
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-05 19:30:53 +0400

    Added handle Function to form ready to make api request

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,11 +2,23 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setarticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+   
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl flex justify-center ">
       {/* search */}
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
           <img
             src={linkIcon}
             alt="link-icon"
@@ -16,8 +28,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Paste the article link"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setarticle({ ...article, url: e.target.value })}
             required
             className=" block w-full rounded-md border:border-gray-200 bg-white py-2.5 pl-10 pr-12 text-sm shadow-lg font-satoshi font-medium focus: border-black focus:outline-none focus:ring-0 peer"
           />
@@ -28,7 +40,10 @@ const Demo = () => {
             <p>↵</p>
           </button>
         </form>
+        {/* browser url history  */}
       </div>
+
+      {/* display results */}
     </section>
   );
 };
