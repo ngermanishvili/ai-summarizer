202821ffb9429134ff0095ca86f1bef3f73b3805
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-05 21:07:18 +0400

    Add local storage functionality to save and retrieve articles.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,14 +1,40 @@
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  const [article, setarticle] = useState({
+  const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
 
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
-   
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticle];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
   };
 
   return (
